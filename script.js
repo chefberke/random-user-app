ed3a3093050adc7e23a4b1b1838f1148e42f33c4
Berke Kanber <berkekanber@gmail.com>
2024-04-29 21:41:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  fetch("https://randomuser.me/api/")
+    .then((data) => {
+      const opacitySettings = document.querySelectorAll(".opacity-settings");
+      const nullTitle = document.querySelector(".null");
+
+      opacitySettings.forEach((opacity) => {
+        opacity.classList.remove("opacity-0");
+      });
+
+      if (nullTitle) {
+        nullTitle.innerHTML = "";
+      }
+      return data.json();
+    })
+    .then((dataReturn) => {
+      document.querySelector(
+        ".name"
+      ).innerHTML = `<strong>Name:</strong> ${dataReturn.results[0].name.first} ${dataReturn.results[0].name.last}`;
+      document.querySelector(
+        ".age"
+      ).innerHTML = `<strong> Age:</strong> ${dataReturn.results[0].dob.age}`;
+      document.querySelector(
+        ".email"
+      ).innerHTML = `<strong>Email:</strong> ${dataReturn.results[0].email}`;
+      document.querySelector(
+        ".phone"
+      ).innerHTML = `<strong>Phone:</strong> ${dataReturn.results[0].phone}`;
+      document.querySelector(
+        ".location"
+      ).innerHTML = `<strong>Location:</strong> 📍${dataReturn.results[0].location.country} / ${dataReturn.results[0].location.city}`;
+      document.querySelector(".image").src =
+        dataReturn.results[0].picture.large;
+    })
+    .then(() => {
+      const date = new Date().getTime();
+      console.log(`${date}usercreated`);
+    })
+    .catch(() => {
+      const opacitySettings = document.querySelectorAll(".opacity-settings");
+      const nullTitle = document.querySelector(".null");
+
+      opacitySettings.forEach((opacity) => {
+        opacity.classList.add("opacity-0");
+      });
+
+      nullTitle.innerHTML = "404: Error <br> Try Again!";
+
+      console.error("404: Error");
+    });
+});
